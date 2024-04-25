129406f016b095822a3e31897a3b0392decf7c85
wu <3340076191@qq.com>
2024-04-25 15:06:25 +0800

    0425

---
+++ src/HelloGitHub.java
@@ -0,0 +1,7 @@
+public class HelloGitHub {
+    public static void main (String [] args) {
+        System.out.println("姓名：XXX");
+        System.out.println("班级：XXX");
+        System.out.println("学号：XXX");
+    }
+}
